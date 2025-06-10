da03784668ab2d680038cc2ab7c643712f412b0f
Suhrud Shringarputale <49031424+SUD58@users.noreply.github.com>
2025-06-10 16:25:08 +0530

    Add GSAP animations to LogoTile component and refactor lighting setup in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,44 @@ import { Canvas } from "@react-three/fiber";
 import "./App.css";
 import { LogoTile } from "./LogoTile";
 import { useRef } from "react";
+import { useHelper } from "@react-three/drei";
+import { SpotLightHelper } from "three";
 
-function App() {
-  const directionalLightRef = useRef();
+function Lights() {
+  const keyLightRef = useRef();
+  const fillLightRef = useRef();
 
+  useHelper(
+    keyLightRef,
+    SpotLightHelper,
+    import.meta.env.NODE_ENV === "development" ? 2 : undefined,
+  );
+  useHelper(
+    fillLightRef,
+    SpotLightHelper,
+    import.meta.env.NODE_ENV === "development" ? 2 : undefined,
+  );
+
+  return (
+    <>
+      <spotLight
+        ref={keyLightRef}
+        position={[10, 8, 10]}
+        intensity={500}
+        castShadow
+      />
+
+      <spotLight
+        ref={fillLightRef}
+        position={[-10, 8, -10]}
+        intensity={300}
+        castShadow
+      />
+    </>
+  );
+}
+
+export default function App() {
   const logoPaths = [
     "apple-tv-logo.webp",
     "disney-plus-logo.webp",
@@ -17,26 +51,26 @@ function App() {
     "paramount-plus-logo.webp",
     "prime-video-logo.webp",
   ];
-  return (
-    <div className="h-svh w-screen bg-white">
-      <Canvas camera={{ position: [-8, 10, -15], fov: 50 }}>
-        <axesHelper args={[5]} />
-        <ambientLight intensity={1} />
-        <directionalLight
-          ref={directionalLightRef}
-          position={[10, 10, 5]}
-          intensity={1}
-        />
-        {directionalLightRef.current && (
-          <directionalLightHelper args={[directionalLightRef.current, 2]} />
-        )}
 
-        {logoPaths.map((logo, index) => (
-          <LogoTile key={index} logo={logo} position={[0, index * -1.25, 0]} />
-        ))}
-      </Canvas>
+  return (
+    <div className="h-[200svh] w-screen">
+      <div className="sticky top-0 h-svh w-screen">
+        <Canvas camera={{ position: [-8, 10, -15], fov: 50 }}>
+          <axesHelper args={[5]} />
+          <Lights />
+          <ambientLight intensity={1} />
+
+          {logoPaths.map((logo, index) => (
+            <LogoTile
+              key={index}
+              logo={logo}
+              position={[0, index * -1.25, 0]}
+              rotation={[0, index * -0.1, 0]}
+              animationDirection={index % 4}
+            />
+          ))}
+        </Canvas>
+      </div>
     </div>
   );
 }
-
-export default App;
--- src/LogoTile.jsx
+++ src/LogoTile.jsx
@@ -1,8 +1,11 @@
 import { useGLTF, useTexture } from "@react-three/drei";
 import * as THREE from "three";
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect, useMemo, useRef } from "react";
+import gsap from "gsap";
 
-export function LogoTile({ logo, position = [0, 0, 0] }) {
+export function LogoTile({ logo, position, rotation, animationDirection }) {
+  const tileRef = useRef();
+
   const { nodes } = useGLTF(import.meta.env.BASE_URL + "logo-tile.glb");
   const texture = useTexture(import.meta.env.BASE_URL + `logos/${logo}`);
   texture.flipY = false;
@@ -65,22 +68,79 @@ export function LogoTile({ logo, position = [0, 0, 0] }) {
   const [logoMat, baseMat] = useMemo(() => {
     const lm = new THREE.MeshPhysicalMaterial({
       map: texture,
-      roughness: 0.2,
+      roughness: 0.6,
       metalness: 0.4,
       toneMapped: false,
     });
     const bm = new THREE.MeshPhysicalMaterial({
       color: avgColor,
       roughness: 0.2,
-      metalness: 0.4,
+      metalness: 0.6,
       toneMapped: true,
     });
     return [lm, bm];
   }, [texture, avgColor]);
 
+  const directionMap = {
+    0: new THREE.Vector3(-10, 0, 0), // left
+    1: new THREE.Vector3(0, 0, -10), // back
+    2: new THREE.Vector3(10, 0, 0), // right
+    3: new THREE.Vector3(0, 0, 10), // front
+  };
+
+  const targetOffset = directionMap[animationDirection] || new THREE.Vector3();
+
+  function handleHover(e) {
+    e.stopPropagation();
+    if (!tileRef.current) return;
+
+    const newPos = tileRef.current.position.clone().add(targetOffset);
+
+    gsap.to(tileRef.current.position, {
+      x: newPos.x,
+      y: newPos.y,
+      z: newPos.z,
+      duration: 0.2,
+      ease: "power2.out",
+    });
+
+    gsap.to(tileRef.current.rotation, {
+      y: Math.PI * 1,
+      duration: 0.2,
+      ease: "power2.out",
+    });
+  }
+
+  function handleUnhover(e) {
+    e.stopPropagation();
+    if (!tileRef.current) return;
+
+    gsap.to(tileRef.current.position, {
+      x: position[0],
+      y: position[1],
+      z: position[2],
+      duration: 0.2,
+      delay: 0.5,
+      ease: "power2.out",
+    });
+
+    gsap.to(tileRef.current.rotation, {
+      y: rotation[1],
+      duration: 0.2,
+      delay: 0.5,
+      ease: "power2.out",
+    });
+  }
   // 4) render both sub-meshes
   return (
-    <group position={position} scale={4}>
+    <group
+      ref={tileRef}
+      position={position}
+      scale={4}
+      rotation={rotation}
+      onPointerOver={handleHover}
+      onPointerOut={handleUnhover}
+    >
       <mesh geometry={nodes.tile_1.geometry} material={logoMat} />
       <mesh geometry={nodes.tile_2.geometry} material={baseMat} />
     </group>
