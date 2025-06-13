31ceb7aea74f1cc7d7b1ac1b757f28cfdbb57df2
Suhrud Shringarputale <49031424+SUD58@users.noreply.github.com>
2025-06-13 18:18:46 +0530

    feat: enhance CTA component styling with improved hover effects and background

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -2,7 +2,7 @@ export function CTA({ header }) {
   return (
     <a
       href=""
-      className={`self-center rounded-full border border-white px-8 py-2 text-white transition hover:bg-white hover:text-black ${header ? "text-sm lg:text-lg" : "text-lg lg:text-2xl xl:text-4xl"}`}
+      className={`relative inline-block self-center rounded-2xl bg-zinc-800 px-8 py-2 text-white transition-colors duration-300 before:transition-colors before:duration-300 hover:bg-white hover:text-black hover:before:from-zinc-300 hover:before:to-zinc-300 ${header ? "text-sm lg:text-lg" : "text-lg lg:text-2xl xl:text-4xl"} before:absolute before:inset-0 before:-z-10 before:-m-0.5 before:rounded-2xl before:bg-conic-110 before:from-black before:from-30% before:via-zinc-300 before:to-black before:to-100%`}
     >
       Join Waitlist
     </a>
