c41ec014b48e6bfda96f13e727c323b19b5bf76d
Suhrud Shringarputale <49031424+SUD58@users.noreply.github.com>
2025-08-01 10:59:28 +0530

    feat: update UIPreviewText component to apply text-balance class for improved styling

--- src/components/UIPreviewSection/UIPreviewText.jsx
+++ src/components/UIPreviewSection/UIPreviewText.jsx
@@ -2,7 +2,7 @@ import React from "react";
 export function UIPreviewText({ heading, children }) {
   return (
     <div className="flex snap-center snap-always flex-col justify-center text-center lg:h-svh lg:text-left">
-      <h3 className="font-heading mb-4 text-2xl font-semibold whitespace-pre-line md:text-3xl lg:text-4xl xl:text-5xl">
+      <h3 className="font-heading mb-4 text-2xl font-semibold text-balance whitespace-pre-line md:text-3xl lg:text-4xl xl:text-5xl">
         {heading}
       </h3>
       <p className="text-lg text-balance text-zinc-300 md:text-xl lg:text-2xl xl:text-3xl">
