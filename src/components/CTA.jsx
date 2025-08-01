ac5352462c981d4eac74f17b48462bfac547cfa5
Suhrud Shringarputale <49031424+SUD58@users.noreply.github.com>
2025-08-01 10:58:50 +0530

    feat: update CTA component styles for consistent header appearance

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -2,13 +2,15 @@ export function CTA({ header }) {
   return (
     <a
       href=""
-      className={`group relative inset-0 z-0 -m-0.5 flex self-center rounded-2xl bg-zinc-800 bg-conic-110 from-black from-35% via-zinc-300 to-black to-100% px-0.5 py-0.5 font-bold tracking-wide text-white transition duration-300 hover:bg-white hover:from-zinc-300 hover:to-zinc-300 hover:text-black ${
+      className={`group relative inset-0 z-0 -m-0.5 flex self-center bg-zinc-800 bg-conic-110 from-black from-35% via-zinc-300 to-black to-100% px-0.5 py-0.5 font-bold tracking-wide text-white transition duration-300 hover:bg-white hover:from-zinc-300 hover:to-zinc-300 hover:text-black lg:rounded-2xl ${
         header
-          ? "text-xs md:text-base lg:text-lg"
-          : "text-base lg:text-lg xl:text-2xl"
+          ? "rounded-xl text-xs md:text-base lg:text-lg"
+          : "rounded-2xl text-base lg:text-lg xl:text-2xl"
       }`}
     >
-      <span className="relative z-10 rounded-2xl bg-zinc-800 px-8 py-2 transition duration-300 group-hover:bg-white group-hover:text-black">
+      <span
+        className={`${header ? "rounded-xl" : "rounded-2xl"} relative z-10 bg-zinc-800 px-8 py-2 transition duration-300 group-hover:bg-white group-hover:text-black lg:rounded-2xl`}
+      >
         Join Waitlist
       </span>
     </a>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { CTA } from "./CTA";
 export function Header() {
   return (
     <header className="fixed top-0 left-0 z-50 flex w-full items-center justify-between px-6 py-4 transition-all duration-300">
-      <img src="StreamSync-Logo.svg" alt="" className="w-48" />
+      <img src="StreamSync-Logo.svg" alt="" className="w-32 lg:w-48" />
       <CTA header />
     </header>
   );
